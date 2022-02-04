27415830e7bd374105c4250fceeee6a6c2161fa2
Noria Naneche <noriananeche@hotmail.com>
2022-02-04 13:52:45 +0100

    ajout de propriétés

--- evaluationgit.php
+++ evaluationgit.php
@@ -6,4 +6,8 @@ class Client
     private string $prenom = "";
     private string $sexe = "";
     private string $adresse = "";
+    private int $tel = 0;
+    private string $dateBirth = ""; 
+    private int $nombreDeCommande = 0;
+    private int $totalDepense = 0;
 }
